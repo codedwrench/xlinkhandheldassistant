f11293bd41fb6b64f758516808a0175190ed00a6
codedwrench <rick.04.1996@gmail.com>
2020-09-13 13:30:54 +0200

    Fix formatting.

--- Tests/PacketConverter_Test.cpp
+++ Tests/PacketConverter_Test.cpp
@@ -4,9 +4,10 @@
  * */
 
 #include "../Includes/PacketConverter.h"
-#include "../Includes/PCapReader.h"
 
 #include <gtest/gtest.h>
+
+#include "../Includes/PCapReader.h"
 
 class PacketConverterTest : public ::testing::Test
 {
@@ -23,21 +24,21 @@ TEST_F(PacketConverterTest, MacToInt)
 
 TEST_F(PacketConverterTest, PromiscuousToMonitor)
 {
-    PCapReader lPCapReader{};
-    PCapReader lPCapExpectedReader{};
-    pcap_t*           lHandler       = pcap_open_dead(DLT_IEEE802_11_RADIO, 65535);
+    PCapReader        lPCapReader{};
+    PCapReader        lPCapExpectedReader{};
+    pcap_t*           lHandler        = pcap_open_dead(DLT_IEEE802_11_RADIO, 65535);
     const std::string lOutputFileName = "../Tests/Output/PromiscuousToMonitorOutput.pcap";
     pcap_dumper_t*    lDumper         = pcap_dump_open(lHandler, lOutputFileName.c_str());
     lPCapReader.Open("../Tests/Input/PromiscuousHelloWorld.pcapng");
     lPCapExpectedReader.Open("../Tests/Input/PromiscuousToMonitorOutput_Expected.pcap");
 
-    while(lPCapReader.ReadNextPacket()) {
+    while (lPCapReader.ReadNextPacket()) {
         std::string lDataToConvert = lPCapReader.DataToString();
-        lDataToConvert = mPacketConverter.ConvertPacketTo80211(lDataToConvert, "01:23:45:67:AB:CD");
+        lDataToConvert             = mPacketConverter.ConvertPacketTo80211(lDataToConvert, "01:23:45:67:AB:CD");
 
         pcap_pkthdr lHeader;
         lHeader.caplen = lDataToConvert.size();
-        lHeader.len = lDataToConvert.size();
+        lHeader.len    = lDataToConvert.size();
 
         // Output a file with the results as well so the results can be further inspected
         pcap_dump((u_char*) lDumper, &lHeader, reinterpret_cast<const u_char*>(lDataToConvert.c_str()));
