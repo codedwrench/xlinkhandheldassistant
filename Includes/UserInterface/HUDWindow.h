a835273e2c87c29bfb3e3122d1d7957f22de683a
codedwrench <rick.04.1996@gmail.com>
2021-04-07 10:28:18 +0200

    Fix default off picture when no on/off.txt present

--- Includes/UserInterface/HUDWindow.h
+++ Includes/UserInterface/HUDWindow.h
@@ -21,7 +21,7 @@ public:
 
 private:
     // If you want nice ascii art, add on/off txt files
-    std::string mOffPicture{"O"};
+    std::string mOffPicture{"    O    "};
     std::string mOnPicture{"( ( O ) )"};
     const std::string* mActivePicture{&mOffPicture};
     std::string mOldConnected{""};
