b1fd7e7574ec2fcedc8527d9cab2b0cfc84e78ff
Rick de Bondt <rick_1996@hotmail.com>
2022-04-13 17:05:29 +0200

    fix: doxygen

--- cmake/adddoxygen.cmake
+++ cmake/adddoxygen.cmake
@@ -26,7 +26,7 @@
 find_package(Doxygen)
 if (DOXYGEN_FOUND)
     # Set input and output files
-    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/doxygen/Doxyfile.in)
+    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/Docs/Doxyfile.in)
     set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
     # Request to configure the file
