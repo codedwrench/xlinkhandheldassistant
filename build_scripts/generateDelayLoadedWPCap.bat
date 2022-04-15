38dfbf2022e6b56922f6d82d24ba940ac6fa7b3e
codedwrench <rick.04.1996@gmail.com>
2022-04-16 00:25:31 +0200

    fix: installing npcap didnt work, copy wpcap.dll instead

--- build_scripts/generateDelayLoadedWPCap.bat
+++ build_scripts/generateDelayLoadedWPCap.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
 if not exist ..\Objects (mkdir ..\Objects)
 cd ..\Objects
-gendef %WINDIR%\System32\Npcap\wpcap.dll
+
+if not defined DLLPath (
+  set DLLPath=%WINDIR%\System32\Npcap
+)
+
+gendef %DLLPath%\wpcap.dll
 dlltool -dllname %WINDIR%\System32\Npcap\wpcap.dll --def wpcap.def --output-delaylib wpcap.a
 cd ..
